048cc25705896f3e538ec7448dcd91c2cd320044
Mikah Mathews <mikah@outofband.org>
2022-01-24 10:17:42 -0800

    Add adressBook, contact, and fullname
    Co-authored-by: Brenna Lavin <lavinbrenna@gmail.com>
    Co-authored by: Andy Plymate <Andy.Plymate@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ function AddressBook() {
   this.contacts = {};
 }
 
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
