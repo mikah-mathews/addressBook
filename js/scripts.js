17e532a975321f323d03a4f781aeaa248ac2af93
Mikah Mathews <mikah@outofband.org>
2022-01-24 11:44:30 -0800

    Add update function
    Co-authored-by: Brenna Lavin <lavinbrenna@gmail.com>
    Co-authored by: Andy Plymate <Andy.Plymate@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,46 @@
+// Business Logic for AddressBook
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function(){
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+AddressBook.prototype.update = function(id, firstName, lastName, phoneNumber) {
+  if (this.contacts[id] === undefined) {
+    return false
+  } else {
+    this.contacts[id].firstName = firstName;
+    this.contacts[id].lastName = lastName;
+    this.contacts[id].phoneNumber = phoneNumber;
+  }
+   
+};
+
+// Business Logic for Contacts
 
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
@@ -11,3 +51,15 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+
+
+
+
+
+let addressBook = new AddressBook();
+
+let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
